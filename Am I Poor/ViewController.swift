3323ca369bcb108bc09d2018ba3178aed5eb0b58
FrancaTM <francatm@outlook.com>
2020-09-18 10:27:30 -0300

    Initial Commit

---
+++ Am I Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Am I Poor
+//
+//  Created by Tulio Marcos Franca on 18/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
